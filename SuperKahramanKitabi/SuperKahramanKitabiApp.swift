69c7e5c209b9a9ed58808c7a3137497116cef22b
muhammed gül <muhammedgul.mail@gmail.com>
2022-10-27 19:05:16 +0300

    Initial Commit

---
+++ SuperKahramanKitabi/SuperKahramanKitabiApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperKahramanKitabiApp.swift
+//  SuperKahramanKitabi
+//
+//  Created by Muhammed Gül on 27.10.2022.
+//
+
+import SwiftUI
+
+@main
+struct SuperKahramanKitabiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
